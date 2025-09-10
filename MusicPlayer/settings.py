a5857c9beb6feaddc56677a5a85c7206978a86c7
ShivamGupta38 <95618860+ShivamGupta38@users.noreply.github.com>
2025-09-10 19:42:53 +0530

    Update settings.py

--- MusicPlayer/settings.py
+++ MusicPlayer/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'SECRET_KEY'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['music-player-1-oscs.onrender.com','*']
 
 
 # Application definition
